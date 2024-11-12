6710e0dcec11b3697863b8478963e88a266c8a9e
azizhamoud35 <dr.abdulaziz35@gmail.com>
2024-11-12 08:17:02 +0300

    logo

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -5,9 +5,9 @@ export function SignupPage() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-secondary/30 px-4 py-8">
       <div className="w-full max-w-md space-y-8">
         <div className="flex flex-col items-center gap-2">
-          <img src="/logo.svg" alt="Namat Logo" className="h-12 w-12" />
+          <img src="/namatclinic.png" alt="Namat Logo" className="h-16 w-26" />
           <h1 className="text-3xl md:text-4xl font-bold text-center bg-gradient-to-r from-[#7dda55] to-[#7dda55]/70 bg-clip-text text-transparent">
-            Namat Clinic
+          
           </h1>
         </div>
         <SignupForm />
